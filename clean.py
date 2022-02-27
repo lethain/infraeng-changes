3541507c595acd5072ebc1c6758e273a84337616
Will Larson <lethain@gmail.com>
2022-02-27 09:41:54 -0800

    Add script to clean website

---
+++ clean.py
@@ -0,0 +1,35 @@
+import sys
+from html.parser import HTMLParser
+
+
+class InfraEngParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.txt = ""
+        self.record = False
+    
+    def handle_starttag(self, tag, attrs):        
+        #print("Encountered a start tag:", tag)
+        if tag == "section":
+            self.record = True
+        
+    def handle_endtag(self, tag):
+        #print("Encountered an end tag :", tag)
+        if tag == "section":
+            self.record = False
+
+    def handle_data(self, data):
+        if self.record and data.strip():
+            self.txt += data.strip() + '\n'
+
+
+def clean(txt):
+    parser = InfraEngParser()
+    parser.feed(txt)
+    return parser.txt
+
+
+if __name__ == "__main__":
+    cleaned = clean(sys.stdin.read())
+    sys.stdout.write(cleaned)
+    sys.stdout.flush()
